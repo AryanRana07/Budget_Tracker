d951b1ba4228c7bdc12812d3fdd8079b6a796025
AryanRana07 <aryanrana0703@gmail.com>
2023-08-25 01:46:37 +0530

    Expenses

---
+++ app/expenses.py
@@ -0,0 +1,6 @@
+class Expenses:
+    def __init__(self,category, date, amount, description):
+        self.categroy = category
+        self.date = date
+        self.amount = amount
+        self.description = description

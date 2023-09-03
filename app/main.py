9943d38b41bd61d2f67e74009a5a6a5a913c1444
AryanRana07 <aryanrana0703@gmail.com>
2023-09-04 05:07:54 +0530

    debug 1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from expense import Expense
 from budget_manager import BudgetManager
 
-expense1 = Expense(category="Groceries, date="2023-08-01", amount = 50, description="Weekly Groceries")
+expense1 = Expense (category="Groceries", date="2023-08-01", amount = 50, description="Weekly Groceries")
 
 budget_manager = BudgetManager()
 
@@ -10,5 +10,3 @@ budget_manager.set_budget(category = "Groceries" , amount = 200)
 print(f"Exense Category: {expense1.category}")
 print(f"Expense Amount: ${expense1.amount}")
 print(f"Budget for {expense1.category}: ${budget_manager.budget.get(expense1.category,'N/A')}")
-  
-  
